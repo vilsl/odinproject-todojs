307eff9568775d18c1b6169ef4deea497dab73de
vilsl <vilsl@fastmail.com>
2020-07-20 11:25:36 +0200

    Finished todo module and API

---
+++ src/renderer.js
@@ -0,0 +1,16 @@
+/* 
+This module handles rendering the lists and todos, then pushes
+them to the DOM.
+*/
+
+// Loops through and parses information from the todo lists
+const ListExtractor = (() => {
+
+})();
+
+// Pushes the information to DOM
+const ListRenderer = (() => {
+
+})();
+
+
--- src/todo.js
+++ src/todo.js
@@ -5,15 +5,15 @@ functions as the API.
 */
 
 // Factory that creates object that holds todos
-const ListObject = () => {
+const ListObject = (todoContent) => {
     let _todoArray = [];
 
-    const addToArray = newTodo => _todoArray.push(newTodo);
-    const removeFromArray = newTodo => _todoArray.pop(newTodo);
+    const addToArray = todoContent => _todoArray.push(todoContent);
+    const removeFromArray = todoContent => _todoArray.pop(todoContent);
     const getArray = () => _todoArray;
 
     return {
-        addToArray,
+        addToArray, 
         removeFromArray,
         getArray
     }
@@ -51,17 +51,79 @@ const TodoObject = (title, description, dueDate, priority) => {
     }
 };
 
-const ListUpdater = (() => {
-// Add to list, remove from list
+// Pushes and removes from the 
+const ListUpdater = ((list, todoContent) => {
+    const pushList = (list, todoContent) => list.addToArray(todoContent);
+    const popList = (list, todoContent) => list.removeFromArray(todoContent);
+
+    return {
+        pushList,
+        popList
+    }
 })();
 
-const TodoUpdater = (() => {
 // Update title, description, due date, priority, complete status
+const TodoUpdater = ((todo, newInfo) => {
+    const changeTitle = (todo, newInfo) => todo.changeTitle(newInfo);
+    const changeDescription = (todo, newInfo) => todo.changeDescription(newInfo);
+    const changeDueDate = (todo, newInfo) => todo.changeDueDate(newInfo);
+    const changePriority = (todo, newInfo) => todo.changePriority(newInfo);
+    const markComplete = (todo) => todo.markComplete();
+    const markNotComplete = (todo) => todo.markNotComplete();
+
+    return {
+        changeTitle,
+        changeDescription,
+        changeDueDate,
+        changePriority,
+        markComplete,
+        markNotComplete
+    }
+})();
+
+// Creates a new todo object  
+const NewTodo = ((title, description, dueDate, priority, list) => { 
+    let newTodo = TodoUpdater(title, description, dueDate, priority);
+    list.addToArray(newTodo);
+})();
+
+// Removes todo object from list array
+const RemoveTodo = ((todo, list) => {
+    list.removeFromArray(todo);
 })();
 
 // Mediator function
-const TodoMediator = (() => {
-
+const TodoMediator = ((command, todoContent, newTodo) => {
+    if (command == "newTodo"){
+        NewTodo(title, description, dueDate, priority, list);
+    }
+    else if (command == "removeTodo"){
+        RemoveTodo(todo, list);
+    }
+    // Toggles 'compete' status of todo object
+    else if (command == "completeTodo"){
+        if (todoContent.getCompleted == false){
+            TodoUpdater.markComplete(todoContent);
+        }
+        else {
+            TodoUpdater.markNotComplete(todoContent);
+        }
+    }
+    // Modifies the todo object with new info
+    else if (command == "updateTodo"){
+        if (todoContent.getTitle() != newTodo.getTitle()){
+            TodoUpdater.changeTitle(todoContent, newTodo.getTitle());
+        }
+        if (todoContent.getDescription() != newTodo.getDescription()){
+            TodoUpdater.changeDescription(todoContent, newTodo.getDescription());
+        }
+        if (todoContent.getDueDate() != newTodo.getDueDate()){
+            TodoUpdater.changeDueDate(todoContent, newTodo.getDueDate());
+        }
+        if (todoContent.getPriority() != newTodo.getPriority()){
+            TodoUpdater.changePriority(todoContent, newTodo.getPriority());
+        }
+    }
 })();
 
 export {TodoMediator as todo};
